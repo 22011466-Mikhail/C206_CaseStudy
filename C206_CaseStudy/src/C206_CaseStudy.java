f6d525de20d2aaa130f77a02ce365d32722e5552
michelle <michelle@10.175.7.233>
2023-08-04 12:38:46 +0800

    asa

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,10 +1,8 @@
 
 public class C206_CaseStudy {
-<<<<<<< HEAD
 //yc???as
-=======
 //jay
->>>>>>> branch 'master' of https://github.com/22011466-Mikhail/C206_CaseStudy.git
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
