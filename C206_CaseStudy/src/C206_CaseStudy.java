15991a032c66ce13bb8ba7d6dc60f0d77bc5bcee
22030683 <22030683@hakim>
2023-08-08 16:39:11 +0800

    add, view, delete Fee : Hakim

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -7,14 +7,16 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		 ArrayList<Fee> feeList = new ArrayList<>();
+	    addFee(feeList, "Tuition Fee", 1000.0, "2023-09-01");
 	}
-	
+
 	public static void setHeader(String header) {
 		Helper.line(80, "-");
 		System.out.println(header);
 		Helper.line(80, "-");
 	}
-	
+
 	public static String showAvailability(boolean isAvailable) {
 		String avail;
 
@@ -25,42 +27,66 @@ public class C206_CaseStudy {
 		}
 		return avail;
 	}
-	
+
 	public static String retrieveAllEnrolment(ArrayList<Enrolment> EnrolmentList) {
 		String output = "";
 
 		for (int i = 0; i < EnrolmentList.size(); i++) {
 			if (EnrolmentList.get(i).getIsAvailable()) {
 				output += String.format("%-10s %-30s %-10s %-10s %-20d\n", EnrolmentList.get(i).getid(),
-						EnrolmentList.get(i).getDescription(), 
+						EnrolmentList.get(i).getDescription(),
 						C206_CaseStudy.showAvailability(EnrolmentList.get(i).getIsAvailable()),
-					EnrolmentList.get(i).getDate(),EnrolmentList.get(i).getOpticalZoom());
+						EnrolmentList.get(i).getDate(), EnrolmentList.get(i).getOpticalZoom());
 			}
 		}
 		return output;
 	}
-	
+
 	public static void viewAllEnrolment(ArrayList<Enrolment> EnrolmentList) {
 		C206_CaseStudy.setHeader("ENROLMENT LIST");
-		String output = String.format("%-10s %-30s %-10s %-10s %-20s\n", "ASSET TAG", "DESCRIPTION",
-				"AVAILABLE", "DUE DATE","OPTICAL ZOOM");
-		 output += retrieveAllEnrolment(EnrolmentList);	
+		String output = String.format("%-10s %-30s %-10s %-10s %-20s\n", "ASSET TAG", "DESCRIPTION", "AVAILABLE",
+				"DUE DATE", "OPTICAL ZOOM");
+		output += retrieveAllEnrolment(EnrolmentList);
 		System.out.println(output);
 	}
-	
+
 	public static void addEnrolment(ArrayList<Enrolment> camcorderList, Enrolment en) {
 		Camcorder Enrolment;
-		for(int i = 0; i < camcorderList.size(); i++) {
+		for (int i = 0; i < camcorderList.size(); i++) {
 			Enrolment = camcorderList.get(i);
-			if (Enrolment.getid().equalsIgnoreCase(en.getid()) )
+			if (Enrolment.getid().equalsIgnoreCase(en.getid()))
 				return;
 		}
-		if ((en.getid().isEmpty()) || (en.getDescription().isEmpty()) ) {
+		if ((en.getid().isEmpty()) || (en.getDescription().isEmpty())) {
 			return;
 		}
-		
+
 		camcorderList.add(en);
 	}
-	
-	
+
+	public static void addFee(ArrayList<Fee> feeList, String feeType, double amount, String dueDate) {
+		Fee newFee = new Fee(feeType, amount, dueDate);
+		feeList.add(newFee);
+		System.out.println("Fee added successfully.");
+	}
+
+	public static void viewAllFees(ArrayList<Fee> feeList) {
+		System.out.println("Fee List");
+		String format = "%-20s %-10s %-15s\n";
+		System.out.printf(format, "FEE TYPE", "AMOUNT", "DUE DATE");
+
+		for (Fee fee : feeList) {
+			System.out.printf(format, fee.getFeeType(), fee.getAmount(), fee.getDueDate());
+		}
+	}
+
+	public static void deleteFee(ArrayList<Fee> feeList, int index) {
+		if (index >= 0 && index < feeList.size()) {
+			feeList.remove(index);
+			System.out.println("Fee deleted successfully.");
+		} else {
+			System.out.println("Invalid fee index.");
+		}
+	}
+
 }
